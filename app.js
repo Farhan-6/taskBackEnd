4b59750910cb47eeed02caa5d458d82596430be5
Farhan-6 <92429701+Farhan-6@users.noreply.github.com>
2025-05-22 21:45:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const taskApis = require('./controller/taskController')
 
 app.use(express.json())
 app.use(cors({
-    origin : ["http://localhost:5173"],
+    origin : ["https://taskify-indol.vercel.app"],
     credentials: true
 }))
 app.use(cookieParser())
